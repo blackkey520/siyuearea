0d595de9b6cab065e7912970887923a78c33071e
NeilyoLee <blackkey520@126.com>
2018-06-17 19:19:08 +0800

    update config

--- config/config.prod.js
+++ config/config.prod.js
@@ -2,7 +2,7 @@ exports.mysql = {
   // 单数据库信息配置
   client: {
     // host
-    host: "localhost",
+    host: "172.21.0.2",
     // 端口号
     port: "3306",
     // 用户名
@@ -10,7 +10,7 @@ exports.mysql = {
     // 密码
     password: "P@ssw0rd",
     // 数据库名
-    database: "testdb"
+    database: "siyuearea"
   },
   // 是否加载到 app 上，默认开启
   app: true,
