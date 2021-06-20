f118d489108abea4715fcd98121af23e4a2e93a9
NeilyoLee <blackkey520@126.com>
2021-06-21 00:02:41 +0800

    update

--- app/controller/pay.js
+++ app/controller/pay.js
@@ -5,15 +5,15 @@ const Controller = require('egg').Controller;
 const middleware = require('../utils/wechat-pay').middleware;
 const Payment = require('../utils/wechat-pay').Payment;
 var initConfig = {
-    partnerKey: '',
-    appId: '',
-    mchId: '',
+    partnerKey: 'wtP77z2jPt85GhXzFKio4mC0lk7fWEV4',
+    appId: 'wx806d517c00b4e3db',
+    mchId: '1507977561',
     notifyUrl: "http://" + this.host + "/notify/"
 };
 var pinitConfig = {
-    partnerKey: '',
-    appId: '',
-    mchId: '',
+    partnerKey: 'wtP77z2jPt85GhXzFKio4mC0lk7fWEV4',
+    appId: 'wxc762f1ccccc82daf',
+    mchId: '1507977561',
     notifyUrl: "http://" + this.host + "/notify/"
 };
 var payment = new Payment(initConfig);
